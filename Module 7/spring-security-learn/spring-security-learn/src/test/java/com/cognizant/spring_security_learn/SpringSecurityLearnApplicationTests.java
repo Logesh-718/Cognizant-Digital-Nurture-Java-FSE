ec927a1330ed343ee11c4c67bb758225dd1e897e
LOGESH Y <logeshyouvarajan@gmail.com>
2026-07-02 10:08:44 +0530

    Add files via upload

---
+++ Module 7/spring-security-learn/spring-security-learn/src/test/java/com/cognizant/spring_security_learn/SpringSecurityLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_security_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
